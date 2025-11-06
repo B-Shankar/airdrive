fe58f99a8273cc91909a8107c2f9b162ed481afd
B-Shankar <learn.bhimashankar@gmail.com>
2025-11-06 22:26:12 +0530

    implemented clerk auth

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Landing from './pages/Landing.jsx';
 import NotFound from "./pages/NotFound.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import {RedirectToSignIn, SignedIn, SignedOut} from "@clerk/clerk-react";
 
 const App = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Landing />} />
+				<Route path="/dashboard" element={
+					<>
+						<SignedIn><Dashboard /></SignedIn>
+						<SignedOut><RedirectToSignIn /></SignedOut>
+					</>
+				} />
 
 				{/* Catch-all route for unmatched paths */}
 				<Route path="*" element={<NotFound />} />
